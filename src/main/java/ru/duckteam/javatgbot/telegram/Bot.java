89c79188ad35b83d63238ab63b82a679077b6dcb
SamuraJey <cergeypro703@gmail.com>
2023-10-17 11:46:55 +0500

    bot purgeee

--- src/main/java/ru/duckteam/javatgbot/telegram/Bot.java
+++ src/main/java/ru/duckteam/javatgbot/telegram/Bot.java
@@ -16,15 +16,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Bot extends TelegramLongPollingBot implements AnswerWriter {
+    private static final Logger LOGS = LoggerFactory.getLogger(Bot.class);
     private final String botName;
     private final MessageConverter reader = new TelegramMessageConverter();
     private final MessageHandler handler = new EchoMessageHandler();
     private final ApiHandler apiHandler = new ApiHandler();
-    private boolean isEcho = false;
-
-    private boolean isEvents = true;
-
-    private static final Logger LOGS = LoggerFactory.getLogger(Bot.class);
+    private final boolean isEcho = false;
+    private final boolean isEvents = true;
 
     public Bot(String apiKey, String botName) {
         super(apiKey);
@@ -34,10 +32,6 @@ public class Bot extends TelegramLongPollingBot implements AnswerWriter {
     @Override
     public void onUpdateReceived(Update update) {
         BotRequest request = reader.convertMessage(update);
-        if (request.getMessage().isCommand()) {
-            commandHandler(request.getMessage().getText());
-            return;                                     // We don't want to echo commands, so we exit
-        }
         handler.handle(request, this);
     }
 
@@ -68,46 +62,14 @@ public class Bot extends TelegramLongPollingBot implements AnswerWriter {
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);      //Any error will be printed here
         }
-
-//        LOGS.info("Получено сообщение ID=%s %s".formatted(response.getMessageId(), response.getUserName()));
     }
-
-//    private void scream(BotResponse response) {
-//        if (response.getMessage().hasText()) {
-//            sendText(response, response.getMessage().getText().toUpperCase());
-//        }
-//    }
 
     @Override
     public void writeAnswer(BotResponse response) {
-        if (isEcho) {
-            copyMessage(response);
-        } else if (isEvents) {
-//            ApiHandler apiHandler = new ApiHandler();
-            try {
-                String answer = apiHandler.getResponse();
-                sendText(response, answer);
-            } catch (Exception e) {
-                LOGS.error("Ошибка при получении ответа от API");
-                //e.printStackTrace();
-            }
-        } else {
-            sendText(response, "Что-то пошло не так.");
-        }
-    }
+//      copyMessage(response);
+//        String answer = apiHandler.getResponse();
+        sendText(response, response.getMessage().getText());
 
-    private void commandHandler(String command) {
-        switch (command) {
-            case "/echo":
-                isEvents = false;
-                isEcho = !isEcho;
-                break;
-            case "/events":
-                isEcho = false;
-                isEvents = !isEvents;
-                break;
-            default:
-                break;
-        }
     }
 }
+
